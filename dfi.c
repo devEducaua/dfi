f81ea474d5f162ef7fc3097bbbd8390e98594ff6
devEducaua <schokito4@gmail.com>
2025-08-12 21:08:08 -0300

    feat: readd idle

--- dfi.c
+++ dfi.c
@@ -5,10 +5,16 @@
 
 void interpreter(char buff[256], FILE* fptr);
 void run(int *accumulator, char buff[]);
+void idle(char buff[256]);
 
 int main(int argc, char* argv[]) {
     char buff[256];
     FILE *fptr;
+
+    if (argc == 1) {
+        idle(buff);
+        return 0;
+    }
 
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] != '-') {
@@ -63,6 +69,40 @@ void run(int *accumulator, char buff[]) {
 
         if (*accumulator == 256 || *accumulator == -1) {
             *accumulator = 0;
+        }
+    }
+}
+
+void idle(char buff[256]) {
+    int accumulator = 0;
+
+    bool quit = false;
+
+    while (!quit) {
+        printf(">> ");
+        fgets(buff, 256, stdin);
+        for (int i = 0; i < 256; i++) {
+            switch(buff[i]) {
+                case 'i':
+                    accumulator += 1;
+                    break;
+                case 'd':
+                    accumulator -= 1;
+                    break;
+                case 's':
+                    accumulator *= accumulator;
+                    break;
+                case 'o':
+                    printf("%d\n", accumulator);
+                    break;
+                case 'q':
+                    quit = true;
+                    break;
+            }
+
+            if (accumulator == 256 || accumulator == -1) {
+                accumulator = 0;
+            }
         }
     }
 }
